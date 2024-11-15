c98f316f5ca3ce811e15a3f40b7170cb3a9c5db7
Amirreza <83154350+AMIR34A@users.noreply.github.com>
2024-11-15 19:15:02 +0330

    Feat: Add SignInController.

---
+++ Unima/Controllers/SignInController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Unima.Controllers
+{
+    public class SignInController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
