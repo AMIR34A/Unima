46aff0d6f6da0e32da58983ec9994690cbd79899
hamidghasemi81 <seyedhamidqasemi99@gmail.com>
2025-02-28 14:44:15 +0330

    Update Verification

--- Unima/wwwroot/js/Verification.js
+++ Unima/wwwroot/js/Verification.js
@@ -68,3 +68,16 @@ function resendOTP() {
 }
 document.getElementById("resend-link").style.display = "none";
 timerInterval = setInterval(updateTimer, 1000);
+
+//token input
+
+let input1=document.getElementById("input1").value
+let input2=document.getElementById("input1").value
+let input3=document.getElementById("input1").value
+let input4=document.getElementById("input1").value
+let input5=document.getElementById("input1").value
+let input6=document.getElementById("input1").value
+let tokenInput=document.getElementById("Token").value
+document.querySelector("#submitButton").addEventListener("submit" , (e)=>{
+  tokenInput=`${input1}${input2}${input3}${input4}${input5}${input6}`
+})
