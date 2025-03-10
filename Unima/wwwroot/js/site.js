0e4955abc44499ef7d230e94e47d0b0f6d3bf98e
hamidghasemi81 <seyedhamidqasemi99@gmail.com>
2025-03-10 15:34:38 +0330

    Update Validation

--- Unima/wwwroot/js/site.js
+++ Unima/wwwroot/js/site.js
@@ -4,47 +4,45 @@ const showSignupBtn = document.getElementById("showSignup");
 const showLoginBtn = document.getElementById("showLogin");
 
 showSignupBtn.addEventListener("click", (event) => {
-    event.preventDefault();
-    setTimeout(() => {
-        loginForm.classList.add("hidden");
-        signupForm.classList.remove("hidden");
-        signupForm.querySelector("input").focus();
-    }, 0);
+  event.preventDefault();
+  setTimeout(() => {
+    loginForm.classList.add("hidden");
+    signupForm.classList.remove("hidden");
+    signupForm.querySelector("input").focus();
+  }, 0);
 });
 showLoginBtn.addEventListener("click", (event) => {
-    event.preventDefault();
-    setTimeout(() => {
-        signupForm.classList.add("hidden");
-        loginForm.classList.remove("hidden");
-        loginForm.querySelector("input").focus();
-    }, 0);
+  event.preventDefault();
+  setTimeout(() => {
+    signupForm.classList.add("hidden");
+    loginForm.classList.remove("hidden");
+    loginForm.querySelector("input").focus();
+  }, 0);
 });
 // Input's Validations
 //Login's Validations
 
 const nameInput = document.getElementById("UserLogInModel_Username");
 nameInput.addEventListener("blur", function () {
-    if (!nameInput.value.trim()) {
-        nameInput.setCustomValidity("Please enter your username.");
-    } else {
-        nameInput.setCustomValidity("");
-    }
+  if (!nameInput.value.trim()) {
+    nameInput.setCustomValidity("لطفا نام کاربری خود را وارد کنید ");
+  } else {
+    nameInput.setCustomValidity("");
+  }
 });
 
 nameInput.addEventListener("input", function () {
-    nameInput.setCustomValidity("");
+  nameInput.setCustomValidity("");
 });
 const passInput = document.getElementById("UserLogInModel_Password");
-passInput.addEventListener("invalid", function () {
-    if (passInput.value === "") {
-        passInput.setCustomValidity(" لطفا   رمز عبور خود را وارد کنید");
-    } else {
-        passInput.setCustomValidity("");
-    }
-});
-passInput.addEventListener("input", function () {
+passInput.addEventListener("blur", function(){
+    if (!passInput.value.trim()){
+        passInput.setCustomValidity("لطفا رمز عبور خود را وارد کنید")
+    }else passInput.setCustomValidity("");
+})
+passInput.addEventListener("input" , ()=>{
     passInput.setCustomValidity("");
-});
+})
 // End Of Login's Validations
 
 //SignUp's Validations
@@ -57,7 +55,7 @@ signupInput.addEventListener("invalid", function () {
   }
 });
 signupInput.addEventListener("input", function () {
-    signupInput.setCustomValidity("");
+  signupInput.setCustomValidity("");
 });
 
 const phoneinput = document.getElementById("phonenumber");
@@ -71,36 +69,38 @@ phoneinput.addEventListener("invalid", function () {
 });
 
 phoneinput.addEventListener("input", function () {
-    phoneinput.setCustomValidity("");
+  phoneinput.setCustomValidity("");
 });
 //End Of SignUp's Validations
 //End Of Validations
 
 function toggleLogInPassword() {
-    var passwordField = document.getElementById("UserLogInModel_Password");
-    var eyeIcon = document.getElementById("logInEyeIcon");
+  var passwordField = document.getElementById("UserLogInModel_Password");
+  var eyeIcon = document.getElementById("logInEyeIcon");
 
-    if (passwordField.type === "password") {
-        passwordField.type = "text";
-        eyeIcon.classList.remove("fa-eye");
-        eyeIcon.classList.add("fa-eye-slash");
-    } else {
-        passwordField.type = "password";
-        eyeIcon.classList.remove("fa-eye-slash");
-        eyeIcon.classList.add("fa-eye");
-    }
+  if (passwordField.type === "password") {
+    passwordField.type = "text";
+    eyeIcon.classList.remove("fa-eye");
+    eyeIcon.classList.add("fa-eye-slash");
+  } else {
+    passwordField.type = "password";
+    eyeIcon.classList.remove("fa-eye-slash");
+    eyeIcon.classList.add("fa-eye");
+  }
 }
 function checkPasswordMatch() {
-    var password = document.getElementById("UserRegisterModel_Password").value;
-    var confirmPassword = document.getElementById("UserRegisterModel_ConfirmPassword").value;
+  var password = document.getElementById("UserRegisterModel_Password").value;
+  var confirmPassword = document.getElementById(
+    "UserRegisterModel_ConfirmPassword"
+  ).value;
 
-    var errorMessage = document.getElementById("error-message");
-    if (password !== confirmPassword) {
-        errorMessage.style.display = "block";
-    } else {
-        errorMessage.style.display = "none";
-    }
+  var errorMessage = document.getElementById("error-message");
+  if (password !== confirmPassword) {
+    errorMessage.style.display = "block";
+  } else {
+    errorMessage.style.display = "none";
+  }
 }
 document.getElementById("Paragraphs").addEventListener("copy", (e) => {
-    e.preventDefault();
+  e.preventDefault();
 });
