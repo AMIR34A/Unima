af9efffa3208dc638078701a7d4c97191a362457
Amirreza <83154350+AMIR34A@users.noreply.github.com>
2025-07-20 19:21:28 +0330

    Add ReserveFood controller and index view.

---
+++ Unima/Areas/User/Controllers/ReserveFoodController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Unima.Areas.User.Controllers;
+
+public class ReserveFoodController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
