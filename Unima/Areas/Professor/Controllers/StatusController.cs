771cf77d11cc7f30fef8b27de5f574348e741ec5
Amirreza <83154350+AMIR34A@users.noreply.github.com>
2025-07-20 15:51:19 +0330

    Add StatusController and view.

---
+++ Unima/Areas/Professor/Controllers/StatusController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Unima.Areas.Professor.Controllers;
+
+public class StatusController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
