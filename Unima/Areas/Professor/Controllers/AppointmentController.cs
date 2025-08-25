d4d29525dc68c5362bf182b2ca178d9fd263dd3f
Amirreza <83154350+AMIR34A@users.noreply.github.com>
2025-08-25 17:48:41 +0330

    Add Appointment controller and index view.

---
+++ Unima/Areas/Professor/Controllers/AppointmentController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Unima.Areas.Professor.Controllers;
+
+public class AppointmentController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
